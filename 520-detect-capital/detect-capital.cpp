5104025af7f749beb045f6f41dd909b88d52e32b
RoopsriMudragada <roopsrimudragada@gmail.com>
2026-08-22 10:53:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (99.93%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.size();
+        int cnt = 0;
+
+        for(auto it : word){
+            if(isupper(it)){
+                cnt++;
+            }
+        }
+        if(cnt == n){
+            return true;
+        }
+        if(cnt == 0){
+            return true;
+        }
+        if(cnt == 1 && isupper(word[0])){
+            return true;
+        }
+        return false;
+    }
+};
