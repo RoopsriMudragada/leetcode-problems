7bdf4ba97d2aafc5b3ca5ef31ccf85bbf4c68709
RoopsriMudragada <roopsrimudragada@gmail.com>
2026-08-22 21:17:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (99.91%) - LeetSync

--- 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.cpp
+++ 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int findLUSlength(string a, string b) {
-        int n = a.size();
-        int m = b.size();
-
-        if(a == b){
-            return -1;
-        }
-        return max(n,m);
+        return a == b ? -1 : max(a.size(), b.size());
     }
 };
