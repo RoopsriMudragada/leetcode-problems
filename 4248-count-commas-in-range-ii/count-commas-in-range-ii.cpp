32345b25aa7b6476f8d9b6bb12f1a72a2f4da63a
RoopsriMudragada <roopsrimudragada@gmail.com>
2026-09-09 15:10:02 +0530

    Time: 2 ms (6.67%) | Memory: 9 MB (57.33%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long result = 0;
+        long long lower = 1000;
+        int comma = 1;
+
+        while(lower <= n){
+            long long upper = (lower * 1000) - 1;
+
+            if(upper > n){
+                upper = n;
+            }
+            
+            long long cnt = upper - lower + 1;
+            result += (cnt * comma);
+
+            lower = lower * 1000;
+            comma += 1;
+        }
+        return result;
+    }
+};
